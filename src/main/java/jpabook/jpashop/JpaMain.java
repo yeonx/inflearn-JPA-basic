66074a8d6fd608a09db6fd8d692c0ab2265c0da9
yeonhee <ydol2@naver.com>
2022-08-29 03:16:16 +0900

    22-08-29 객체지향 쿼리 언어

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -22,18 +22,18 @@ public class JpaMain {
 //           book.setAuthor("김영한");
 //
 //           em.persist(book);
-            Member member = new Member();
-            member.setName("member1");
-            member.setAge(10);
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc",Member.class)
-                    .setFirstResult(10)
-                    .setMaxResults(20)
-                    .getResultList();
+//            Member member = new Member();
+//            member.setName("member1");
+//            member.setAge(10);
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
+//
+//            List<Member> result = em.createQuery("select m from Member m order by m.age desc",Member.class)
+//                    .setFirstResult(10)
+//                    .setMaxResults(20)
+//                    .getResultList();
 
 //            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username,m.age) from Member m",MemberDTO.class)
 //                    .getResultList();
@@ -46,6 +46,26 @@ public class JpaMain {
 //
 //            Query query3 = em.createQuery("select m.name, m.age from member m");
 
+            /**
+             * chapter 10
+             */
+            String query = "select " +
+                    "case when m.age <= 10 then '학생요금' "+
+                    "   when m.age >= 60 then '경로요금' "+
+                    "   else '일반요금' " +
+                    "end " +
+                    "from Member m";
+            em.createQuery(query);
+
+            String query1 = "select 'a' || 'b' From Member m";
+            String query2 = "select group_concat(m.username) From Member m";
+
+            List<String> result = em.createQuery(query2,String.class)
+                            .getResultList();
+
+            for(String s : result){
+                System.out.println("s = " + s);
+            }
 
             tx.commit();
         } catch (Exception e){
